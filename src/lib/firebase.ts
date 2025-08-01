826006fc042d196450b753609b1b388c48c872cb
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-01 18:26:00 +0000

    > git pull --tags origin master From https://github.com/Mojiibrsm/RoktoD

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCEUQbfu7bP20JS9SHGIo88QjoYTASSQlE",
@@ -18,6 +17,5 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app);
 
-export { app, auth, db, storage };
+export { app, auth, db };
