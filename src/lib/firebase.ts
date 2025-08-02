68bef2d4d47bf457fbea00ddaaba59dbbe9b887b
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-02 16:11:29 +0000

    ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -18,14 +18,15 @@ let app: FirebaseApp;
 let auth: Auth;
 let db: Firestore;
 
-if (getApps().length === 0) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApp();
+if (typeof window !== 'undefined') {
+  if (getApps().length === 0) {
+    app = initializeApp(firebaseConfig);
+  } else {
+    app = getApp();
+  }
+  auth = getAuth(app);
+  db = getFirestore(app);
 }
-
-auth = getAuth(app);
-db = getFirestore(app);
 
 
 export { app, auth, db };
