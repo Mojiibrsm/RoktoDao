3182006e8cb66bbd295c12926116630c32c263d6
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-02 06:46:28 +0000

    [12:34:42.673] Running build in Washington, D.C., USA (East) – iad1 [12:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 
+import 'dotenv/config'
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
