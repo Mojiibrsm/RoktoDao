9e2740190915db367a91abfa2411333cc8a30eb1
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-04 21:59:38 +0000

    Error: Expected first argument to collection() to be a CollectionReferen

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,5 @@
 
-import { getApps, initializeApp, getApp, App, cert } from 'firebase-admin/app';
+import { getApps, initializeApp, getApp, App } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import { serviceAccount } from './firebase-service-account';
@@ -7,12 +7,16 @@ import { serviceAccount } from './firebase-service-account';
 const appName = 'firebase-admin-app';
 let adminApp: App;
 
-if (!getApps().some(app => app.name === appName)) {
+if (!getApps().length) {
   adminApp = initializeApp({
-    credential: cert(serviceAccount),
-  }, appName);
+    credential: {
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key,
+    },
+  });
 } else {
-  adminApp = getApp(appName);
+  adminApp = getApp();
 }
 
 const adminAuth = getAuth(adminApp);
