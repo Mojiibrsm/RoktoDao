ad5aaef912fed38c91b3d736a6fa4bfc176a2046
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-05 11:22:50 +0000

    // Import the functions you need from the SDKs you need import { initial

--- next.config.ts
+++ next.config.ts
@@ -39,12 +39,12 @@ const nextConfig: NextConfig = {
     unoptimized: true
   },
   env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: "YOUR_API_KEY",
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyCEUQbfu7bP20JS9SHGIo88QjoYTASSQlE",
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "mailjet-express.firebaseapp.com",
     NEXT_PUBLIC_FIREBASE_PROJECT_ID: "mailjet-express",
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "mailjet-express.appspot.com",
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "420317579893",
-    NEXT_PUBLIC_FIREBASE_APP_ID: "1:420317579893:web:e0f49a5e427e02597f8287",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "mailjet-express.firebasestorage.app",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "1041255436136",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:1041255436136:web:9a2e9eb00211619d64d0dd",
     NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY: "public_mZ0R0Fsxxuu72DflLr4kGejkwrE=",
     NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT: "https://ik.imagekit.io/uekohag7w",
     NEXT_PUBLIC_SITE_URL: "https://roktodao.bartanow.com"
@@ -52,4 +52,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
 
