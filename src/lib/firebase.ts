04054d5bdbad1202f9901370d7d53aee2be14704
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-05 13:01:24 +0000

    ekno same problem, honmepage and sob jaigai. nmone hocche firebase er sa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth, Auth, RecaptchaVerifier } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCEUQbfu7bP20JS9SHGIo88QjoYTASSQlE",
+  authDomain: "mailjet-express.firebaseapp.com",
+  projectId: "mailjet-express",
+  storageBucket: "mailjet-express.appspot.com",
+  messagingSenderId: "1041255436136",
+  appId: "1:1041255436136:web:9a2e9eb00211619d64d0dd"
 };
 
 
