ba8c44b909823309e02e9c0cb40f946c17afd265
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-08 12:50:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,35 +1,19 @@
 
-import { getApps, initializeApp, getApp, App, credential } from 'firebase-admin/app';
+import admin from 'firebase-admin';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
 import serviceAccountJson from './firebase-service-account.json';
 
 // Since the service account is a JSON object, we need to cast it to the correct type.
-const serviceAccount = serviceAccountJson as {
-  type: string;
-  project_id: string;
-  private_key_id: string;
-  private_key: string;
-  client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
-  universe_domain: string;
-};
+const serviceAccount = serviceAccountJson as admin.ServiceAccount;
 
-
-let adminApp: App;
-
-if (!getApps().length) {
-  adminApp = initializeApp({
-    credential: credential.cert(serviceAccount),
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
   });
-} else {
-  adminApp = getApp();
 }
 
+const adminApp = admin.app();
 const adminAuth = getAuth(adminApp);
 const adminDb = getFirestore(adminApp);
 
