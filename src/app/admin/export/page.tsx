46632c75af477e4216165f9bc38d997ebb2143aa
Mojib Rsm <mojibrsm1@gmail.com>
2025-08-09 11:37:15 +0000

    এই সমস্যা বার বার আসতেছে

--- src/app/admin/export/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file has been removed as it is no longer needed after migrating to Supabase.
-// It was used for exporting data from Firestore.
--- src/lib/firebase-service-account.ts
+++
@@ -1,3 +0,0 @@
-// This file is deprecated and has been replaced by firebase-service-account.json
-// It can be safely deleted.
-export {};
